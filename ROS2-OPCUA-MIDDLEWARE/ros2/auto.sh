e958ef1c59131c21c87891e5194dfbff86a585c9
Danielrloken <31891261+Danielrloken@users.noreply.github.com>
2022-04-25 13:46:11 +0200

    Add files via upload

---
+++ ROS2-OPCUA-MIDDLEWARE/ros2/auto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~/ROS2-OPCUA-MIDDLEWARE/ros2
+git pull
+bash build.sh source_ prod
+
+exit 0
---
+++ ROS2-OPCUA-MIDDLEWARE/ros2/kmr_communication/kmr_communication/launch/hybrid.launch.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import yaml
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description(argv=sys.argv[1:]):
+    pkg_name = 'kmr_communication'
+
+    config_file_path = os.path.join(
+            get_package_share_directory(pkg_name),
+            'config',
+            'bringup.yaml')
+    config_file = open(config_file_path)
+    parsed_config_file = yaml.load(config_file, Loader=yaml.FullLoader)
+    
+    hybrid_node_params = parsed_config_file['hybrid_node']['ros__parameters']
+
+    param_dir = LaunchConfiguration(
+        'param_dir',
+        default=config_file_path
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+                'param_dir',
+                default_value=param_dir,
+                description="Full path to parameter file to load"
+            ),
+
+        Node(
+            package=pkg_name,
+            executable='opcua_ros2_hybrid',
+            name='hybrid_node',
+            output='screen',
+            emulate_tty=True,
+            arguments=['-d', hybrid_node_params['domain']]
+        )
+    ])
