497764c351e38702d809a6953ebb7d02fcfa117d
Danielrloken <danielr.looken@gmail.com>
2022-04-30 17:07:27 +0200

    Added missing files from TPK4960

---
+++ ros2/kmr_communication/setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'kmr_communication'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(package_name + '/launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob(package_name + '/config/*.yaml'))
+    ],
+    scripts=[
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mathias',
+    maintainer_email='mathias.neslow96@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'opcua_ros2_hybrid = kmr_communication.nodes.opcua_ros2_pubsub:main',
+        ],
+    },
+)
